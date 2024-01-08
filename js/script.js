8ee9c09fe323e9d713b06c76fca6b89f9296754e
KEPPURAJA E <132921416+keppuraja3@users.noreply.github.com>
2024-01-08 10:55:04 +0530

    Upload main file source 2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Declaration variables:------
+
+const sideNavbar = document.getElementById("sideNavbar");
+
+function ShowNavbar() {
+  sideNavbar.style.width = "80%";
+}
+function hideNavbar() {
+  sideNavbar.style.width = "0";
+}
